f5d3200631bcd58f15fa740514dd660ef39e2a1b
Роман Кузьмин <archer039@MacBook-Pro-Roman.local>
2026-04-08 14:43:06 +0200

    add functions getUserValue() and getConvertedValue()

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USD_TO_EUR float64 = 0.93
 	const USD_TO_RUB float64 = 87.34
 	const EUR_TO_RUB float64 = USD_TO_RUB / USD_TO_EUR
+}
+
+func getUserValue() float64 {
+	var userValue float64
+
+	fmt.Print("Введите значение: ")
+	fmt.Scan(&userValue)
+
+	return userValue
+}
+
+func getConvertedValue(value float64, fromCurrency string, toCurrency string) float64 {
+
 }
